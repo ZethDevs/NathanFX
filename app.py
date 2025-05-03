f92a8c2dd3e5d9351b17270805b331cb7ef86c31
Lutfi Farid <lutfi.faridd@gmail.com>
2025-05-03 18:42:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -211,7 +211,7 @@ def index():
 @app.route('/status')
 def status(): return jsonify({'status':'Online'})
 
-def run_flask(): app.run(host='0.0.0.0',port=5000)
+def run_flask(): app.run(host='0.0.0.0',port=8080)
 
 if __name__=='__main__':
     threading.Thread(target=run_flask,daemon=True).start()
